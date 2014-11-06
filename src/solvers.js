ae8df4b426b1a4707c12971c7cf525b0fe2f2827
nmandel <nrmandel@ucdavis.edu>
2014-11-05 17:12:42 -0800

    Solvers findNRooks solution complete

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,44 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = new Board({n:n});
+  //console.log(solution(4));
+  var list = [];
+  for(var t = 0; t<n; t++){
+    list.push(0);
+  }
+  // for(var i = 0; i<n; i++){
+  //   solution.push([]);
+  //   for(var k = 0; k<n; k++){
+  //     solution[i].push(0);
+  //   }
+  // }
+  for(var j = 0; j<n; j++){
+    for(var l = 0; l<n; l++){
+      if(solution.get(j)[l] === 0){
+        list[l] = 1;
+        solution.set(j, list);
+        //for (var i = 0; i < n; i++) {console.log(solution.get(i))}
+        // console.log(" ");
+        if(solution.hasAnyRowConflicts() || solution.hasAnyColConflicts()){
+            list[l] = 0;
+            solution.set(j, list);
+        }
+
+      }
+    }
+    list = [];
+    for(var t = 0; t<n; t++){
+      list.push(0);
+    }
+  }
+  var result = [];
+  for (var p = 0; p < n; p++) {
+    result.push(solution.get(p));
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return result;
 };
 
 
